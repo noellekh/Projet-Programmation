c44daec80cb7174e9372c6e129205518c6f2a4a8
noellekh <63249943+noellekh@users.noreply.github.com>
2020-08-11 17:44:08 +0200

    Update projet.js

---
+++ Frontend/js/projet.js
@@ -0,0 +1,16 @@
+function start(page){
+  hide(page);
+}
+
+
+function hide(page){
+  document.getElementById('bout_moi').style.display="none";
+  document.getElementById('nutrition').style.display="none";
+  document.getElementById('sport').style.display="none";
+  document.getElementById('bienEtre').style.display="none";
+  document.getElementById('contacts').style.display="none";
+  document.getElementById('acceuil').style.display="none";
+  document.getElementById(page).style.display="inline";
+
+
+}
