ee7a9c675cf57b5868be36fb0cac7b4128b8275e
noellekh <63249943+noellekh@users.noreply.github.com>
2020-08-24 11:34:35 +0200

    Update choixSport.js

--- Frontend/js/choixSport.js
+++ Frontend/js/choixSport.js
@@ -1,15 +1,15 @@
 "use strict !";
 
 let programme=[
-  ["1", "des fentes avant", "15 répétitions/jambes", 1 ],
-  ["2", "20 pompes, 20 abdos, 20 jump and jack", "3 circuits", 1 ],
-  ["3", "des squats", "20 répétitions", 1 ],
-  ["4", "15pompes, 15 abdos, 15 jump and jack", "5 circuits", 2 ],
-  ["5", "la planche", "1 min", 2 ],
-  ["6", "des burpees", "15 répétitions", 2],
-  ["7", "des burpees + pompes", "2 X 15 répétitions", 3 ],
-  ["8", "20 pompes, 20 abdos, 20 jump and jack", "8 circuits", 3 ],
-  ["9", "des squats and jump", "2 X 15 répétitions", 3 ],
+  ["des fentes avant", "15 répétitions/jambes", 1 ],
+  ["20 pompes, 20 abdos, 20 jump and jack", "3 circuits", 1 ],
+  ["des squats", "20 répétitions", 1 ],
+  ["15pompes, 15 abdos, 15 jump and jack", "5 circuits", 2 ],
+  ["la planche", "1 min", 2 ],
+  ["des burpees", "15 répétitions", 2],
+  ["des burpees + pompes", "2 X 15 répétitions", 3 ],
+  ["20 pompes, 20 abdos, 20 jump and jack", "8 circuits", 3 ],
+  ["des squats and jump", "2 X 15 répétitions", 3 ],
 
 ]
 
@@ -22,18 +22,18 @@ function gym(){
 
 
     if(exp == 0){
-      if(programme[i][3] == "1"){
+      if(programme[i][2] == "1"){
 
-        document.getElementById('afaire').innerHTML = '<p>' + 'Vous allez faire ' + programme[1][1] + ' en tout  ' + programme[1][2] + '</p>';
+        document.getElementById('afaire').innerHTML = '<p>' + 'Vous allez faire ' + programme[1][0] + ' en tout  ' + programme[1][1] + '</p>';
         break;
       }
     }
 
 
     else if(exp == 1){
-        if(programme[i][3] == "2"){
+        if(programme[i][2] == "2"){
 
-          document.getElementById('afaire').innerHTML = '<p>' + 'Vous allez faire ' + programme[3][1] + ' en tout  ' +programme[3][2] + '</p>';
+          document.getElementById('afaire').innerHTML = '<p>' + 'Vous allez faire ' + programme[3][0] + ' en tout  ' +programme[3][1] + '</p>';
           break;
 
 
@@ -41,9 +41,9 @@ function gym(){
     }
 
     else if(exp == 2){
-        if(programme[i][3] == "3"){
+        if(programme[i][2] == "3"){
 
-          document.getElementById('afaire').innerHTML = '<p>' + 'Vous allez faire:' + ' ' + programme[7][1] + ' ' + ' en tout ' + programme[7][2] + '</p>';
+          document.getElementById('afaire').innerHTML = '<p>' + 'Vous allez faire:' + ' ' + programme[7][0] + ' ' + ' en tout ' + programme[7][1] + '</p>';
           break;
 
         }
@@ -55,11 +55,55 @@ function gym(){
 }
 
 
+/**
+ * Choisir au hasard un exercice dans le tableau.
+ *
+ * @param {*} programme - reçoi un array d'exercices
+ *
+ * @return {*} Renvoie un élement du tableau pris au hasard.
+ */
+
 
 function rand_om(){
 
   let randPro = programme[Math.floor(Math.random()*programme.length)];
-  document.getElementById('afaire').innerHTML = '<p>' + 'Allez hop   '+ randPro[1] + ' et en tout  ' + randPro[2] +  '</p>';
+  document.getElementById('afaire').innerHTML = '<p>' + 'Allez hop   '+ randPro[0] + ' et en tout  ' + randPro[1] +  '</p>';
+  console.log(typeof randPro);
+
+
+
+}
+
+
+
+
+
+
+function ajouter(){
+  let myFit;
+  let ex_ercices, nbFois, diffic;
+  ex_ercices = document.getElementById('fit').value;
+  nbFois = document.getElementById('repet').value;
+  diffic = document.getElementById('monNiveau');
+  let myLevel = diffic.options.selectedIndex;
+  myFit = new Array (ex_ercices, nbFois, myLevel);
+
+  myFit[0]=ex_ercices;
+  myFit[1]=nbFois;
+  myFit[2]=myLevel + 1;
+
+  console.log(typeof myFit);
+  programme.push(myFit);
+  console.log(programme);
+
+  document.getElementById("mine").innerHTML += '<ul>' +'<li>' + myFit.join("-") + '</li>' +'</ul>';
+
+
+
+
+
+
+
 
 
 
